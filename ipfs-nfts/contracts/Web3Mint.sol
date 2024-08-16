6b49a9dc786705f32894dab43f6e9d99817b387d
akira.nieda <akira.nieda@GX2191.local>
2024-08-16 16:10:40 +0900

    first commit

---
+++ ipfs-nfts/contracts/Web3Mint.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+import "hardhat/console.sol";
+contract Web3Mint {
+    constructor() {
+        console.log("This is my NFT contract!!!");
+    }
+}
